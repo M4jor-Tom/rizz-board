42cb4e8d85330f65823b78b07221af48b89dcfbc
Tom Vautray <tom-vautray@hotmail.fr>
2024-08-09 23:48:29 +0200

    Remove unsed code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import CardList from './card-list/CardList';
 import { User } from './types/user';
@@ -18,21 +17,6 @@ function App() {
       <CardList
         user={user}
       />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-
     </div>
   );
 }
