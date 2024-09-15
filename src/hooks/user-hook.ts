a078cb46dc7c77d30a5854a1dcfc1b395c3888b8
Tom Vautray <tom-vautray@hotmail.fr>
2024-09-15 21:39:19 +0200

    Add useWebSocket hook to hooks

--- src/hooks/user-hook.ts
+++ src/hooks/user-hook.ts
@@ -1,3 +1,4 @@
+import { useState, useEffect, useCallback } from 'react';
 import { User } from "../types/user";
 
 export async function fetchUsers(): Promise<User> {
@@ -7,4 +8,50 @@ export async function fetchUsers(): Promise<User> {
     // const response = await fetch('https://randomuser.me/api');
     const data = await response.json();
     return data;
+}
+
+export const useWebSocket = (url: string) => {
+    const [ws, setWs] = useState<WebSocket | null>(null);
+    const [user, setUser] = useState<User | undefined>(undefined);
+
+    useEffect(() => {
+
+        // Initialiser une nouvelle connexion WebSocket
+        const websocket = new WebSocket(url);
+        setWs(websocket);
+
+        // Fonction pour gérer les messages entrants
+        websocket.onmessage = (event) => {
+            const user = JSON.parse(event.data) as User;
+            setUser(user);
+        };
+        
+        websocket.onerror = (event) => {
+            console.error("WebSocket error:", event);
+        };
+        
+        websocket.onclose = (event) => {
+            console.log("WebSocket connection closed:", event);
+        };
+
+        // Nettoyer lors du démontage du composant
+        return () => {
+
+            websocket.close();
+        };
+    }, [url]);
+
+    // Envoyer des messages
+    // const sendUser = useCallback(
+
+    //     (user: User) => {
+
+    //         if (ws) {
+    //             ws.send(JSON.stringify(user));
+    //         }
+    //     },
+    //     [ws]
+    // );
+
+  return user;
 }
